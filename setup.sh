fc759aebb12268317f94a429e4a9b53447c6237f
rokki91 <117232687+rokki91@users.noreply.github.com>
2022-11-02 03:33:30 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,7 +19,7 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Link Hosting Kalian Untuk Ssh Vpn
-akbarvpn="raw.githubusercontent.com/fisabiliyusri/Mantap/main/ssh"
+akbarvpn="raw.githubusercontent.com/rokki91/Mantap/main/ssh"
 # Link Hosting Kalian Untuk Sstp
 akbarvpnn="raw.githubusercontent.com/fisabiliyusri/Mantap/main/sstp"
 # Link Hosting Kalian Untuk Ssr
